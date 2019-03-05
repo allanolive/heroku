11b561721d186f294af1b240688a4745ed657565
allanolive <allan.loveforfood@gmail.com>
2019-03-05 14:50:06 -0500

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_162821) do
+ActiveRecord::Schema.define(version: 2019_03_05_191008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_162821) do
     t.string "unit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -70,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_162821) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "patients", "doctors"
   add_foreign_key "recipes", "ingredients", column: "ingredients_id"
 end
